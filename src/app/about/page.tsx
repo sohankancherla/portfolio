3101389403ecede70c264ee233f9097d170bd040
skancher <sohank@gmail.com>
2024-09-02 18:13:10 -0700

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,125 @@
+import { type Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import { GitHubIcon, LinkedInIcon } from '@/components/SocialIcons'
+import portraitImage from '@/images/portrait.jpg'
+
+function SocialLink({
+  className,
+  href,
+  children,
+  icon: Icon,
+}: {
+  className?: string
+  href: string
+  icon: React.ComponentType<{ className?: string }>
+  children: React.ReactNode
+}) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        target="_blank"
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'About',
+  description: 'I’m Sohan Kancherla and I love building websites.',
+}
+
+export default function About() {
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+        <div className="lg:pl-20">
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src={portraitImage}
+              alt="Portrait of Sohan Kancherla"
+              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+            I&apos;m Sohan Kancherla and I love building websites.
+          </h1>
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>
+              I&apos;ve always been a builder at heart. My journey began with
+              Lego blocks, crafting everything from towering castles to
+              intricate spaceships. I later transitioed to creating games in
+              Scratch with its simple drag-and-drop interface. This early love
+              for building sparked a passion that would guide my future.
+            </p>
+            <p>
+              In high school, I had the opportunity to intern at Intel. It was
+              like stepping into a whole new world. It was here where I became a
+              real problem solver.
+            </p>
+            <p>
+              Sometime later, I stumbled upon CS50 lectures by David Malan. He
+              taught me the fundamentals of computer science and how to think
+              like a programmer. This is where I discoverd HTML, CSS, and
+              JavaScript. Eager to learn more, I transitioned to The Odin
+              Project, where I practice Full Stack Development.
+            </p>
+            <p>
+              I&apos;m still at Intel, applying everything I&apos;ve learned. My
+              work in frontend development has brought significant improvements,
+              earning positive feedback for enhancing both efficiency and user
+              experience. I&apos;ve found my passion in this field, and I&apos;m
+              excited to continue growing and learning in my career.
+            </p>
+          </div>
+        </div>
+        <div className="lg:pl-20">
+          <ul role="list">
+            <SocialLink
+              href="https://github.com/sohankancherla"
+              icon={GitHubIcon}
+              className="mt-4"
+            >
+              Follow on GitHub
+            </SocialLink>
+            <SocialLink
+              href="https://www.linkedin.com/in/sohan-kancherla/"
+              icon={LinkedInIcon}
+              className="mt-4"
+            >
+              Follow on LinkedIn
+            </SocialLink>
+            <SocialLink
+              href="mailto:sohank@gmail.com"
+              icon={MailIcon}
+              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+            >
+              sohank@gmail.com
+            </SocialLink>
+          </ul>
+        </div>
+      </div>
+    </Container>
+  )
+}
